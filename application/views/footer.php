9882091a462fd6518d5c015f14465c630d25241a
WT <wangte168@sina.com>
2013-06-03 17:09:07 +0800

    修改部分界面

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,10 @@
 	<p>
     CopyRight &copy; 2011-2013 华智讯教育培训公司 All Rights Reserved<br />
     地址：北京市海淀区上地XX小区5单元400室 &nbsp;&nbsp;联系电话：010-xxxxxxxx 传真：010-xxxxxxxx<br />
-    <a href="<?php echo base_url('index.php?d=admin&c=index');?>" target="_blank">管理员登陆</a>
+    <a href="<?php echo base_url('');?>" target="_blank">免责声明</a>&nbsp;&nbsp;|&nbsp;&nbsp; 
+    <a href="<?php echo base_url('index.php?d=admin&c=index');?>" target="_blank">管理登录</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+    <a href="<?php echo base_url('');?>" target="_blank">联系我们</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+    <a href="<?php echo base_url('');?>" target="_blank">关于我们</a>
     </p>
 </div>
 </body>
