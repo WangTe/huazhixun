9f5440fcf989d6cb49ca1beea5346e9121c960d1
wanglin <249719665@qq.com>
2013-06-06 20:12:00 +0800

    修改模板后

---
+++ application/views/new_index.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title>华智讯教育培训中心</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="imagetoolbar" content="no" />
+<link rel="stylesheet" href="static/common/css/layout.css" type="text/css" />
+<script type="text/javascript" src="static/common/js/jquery-1.4.1.min.js"></script>
+<script type="text/javascript" src="static/common/js/jquery.slidepanel.setup.js"></script>
+<script type="text/javascript" src="static/common/js/jquery.cycle.min.js"></script>
+<script type="text/javascript" src="static/common/js/jquery.cycle.setup.js"></script>
+</head>
+<body>
+<!-- <div style="width:728px;height:90px;margin:0 auto;"><script src="http://www.cssmoban.com/include/new/ggad_728x90.js"></script></div>
+<div class="wrapper col0">
+  <div id="topbar">
+    <div id="slidepanel">
+      <div class="topbox">
+        <h2>Nullamlacus dui ipsum</h2>
+        <p>Nullamlacus dui ipsum conseque loborttis non euisque morbi penas dapibulum orna. Urnaultrices quis curabitur phasellentesque congue magnis vestibulum quismodo nulla et feugiat. Adipisciniapellentum leo ut consequam ris felit elit id nibh sociis malesuada.</p>
+        <p class="readmore"><a href="#">Continue Reading &raquo;</a></p>
+      </div>
+      <div class="topbox">
+        <h2>Teachers Login Here</h2>
+        <form action="#" method="post">
+          <fieldset>
+            <legend>Teachers Login Form</legend>
+            <label for="teachername">Username:
+              <input type="text" name="teachername" id="teachername" value="" />
+            </label>
+            <label for="teacherpass">Password:
+              <input type="password" name="teacherpass" id="teacherpass" value="" />
+            </label>
+            <label for="teacherremember">
+              <input class="checkbox" type="checkbox" name="teacherremember" id="teacherremember" checked="checked" />
+              Remember me</label>
+            <p>
+              <input type="submit" name="teacherlogin" id="teacherlogin" value="Login" />
+              &nbsp;
+              <input type="reset" name="teacherreset" id="teacherreset" value="Reset" />
+            </p>
+          </fieldset>
+        </form>
+      </div>
+      <div class="topbox last">
+        <h2>Pupils Login Here</h2>
+        <form action="#" method="post">
+          <fieldset>
+            <legend>Pupils Login Form</legend>
+            <label for="pupilname">Username:
+              <input type="text" name="pupilname" id="pupilname" value="" />
+            </label>
+            <label for="pupilpass">Password:
+              <input type="password" name="pupilpass" id="pupilpass" value="" />
+            </label>
+            <label for="pupilremember">
+              <input class="checkbox" type="checkbox" name="pupilremember" id="pupilremember" checked="checked" />
+              Remember me</label>
+            <p>
+              <input type="submit" name="pupillogin" id="pupillogin" value="Login" />
+              &nbsp;
+              <input type="reset" name="pupilreset" id="pupilreset" value="Reset" />
+            </p>
+          </fieldset>
+        </form>
+      </div>
+      <br class="clear" />
+    </div>
+    <div id="loginpanel">
+      <ul>
+        <li class="left">Log In Here &raquo;</li>
+        <li class="right" id="toggle"><a id="slideit" href="#slidepanel">Administration</a><a id="closeit" style="display: none;" href="#slidepanel">Close Panel</a></li>
+      </ul>
+    </div>
+    <br class="clear" />
+  </div>
+</div> -->
+<!-- ####################################################################################################### -->
+<div class="wrapper col1">
+  <div id="header">
+    <div id="logo">
+      <h1><a href="#">华智讯教育培训中心</a></h1>
+      <p>华智讯,华智讯培训,教育培训</p>
+    </div>
+    <div id="topnav">
+      <ul>
+        <li class="active"><a href="#">首页</a></li>
+        <li><a href="#">讲师</a></li>
+        <li><a href="#">课程</a></li>
+        <li><a href="#">项目</a></li>
+        <li><a href="#">动态</a></li>
+        <li><a href="#">公益</a></li>
+        <li class="last"><a href="#">关于</a></li>
+      </ul>
+    </div>
+    <br class="clear" />
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper col2">
+  <div id="featured_slide">
+    <div class="featured_box"><a href="#"><img src="static/common/img/student_1.jpg" alt="" /></a>
+      <div class="floater">
+        <h2>1. 塑造团队高效执行力</h2>
+        <p>这一系列的问题困扰着每一个管理者，有没有一种管理模式，可以激活团队和组织，使员工能够创造性工作，而不是被动完成任务；使员工能够主动超越目标，而不是应付考核；《团队高效协作与执行管理模式》从组织、文化、人性层面对企业管理过程中面临的实际问题进行深刻剖析，实现“全员、全方位、全过程”的高效能团队管理模式，推动企业管理升级。</p>
+        <p class="readmore"><a href="#">继续阅读 &raquo;</a></p>
+      </div>
+    </div>
+    <div class="featured_box"><a href="#"><img src="static/common/img/student_2.jpg" alt="" /></a>
+      <div class="floater">
+        <h2>2. 卓越班组建设与班组长管理能力提升</h2>
+        <p>现实中这一系列现象令领导头疼，并严重制约着班组发展与绩效成长，而现象背后反映出的则是班组长在管理中缺乏一套行之有效、能够解决以上诸多实际问题的管理模式和管理方法，《如何打造卓越班组》课程全程案例教学，从班组管理中的实际问题入手，提供系统化解决方案：有模式、有机制、有工具、有具体使用方法与技巧，从而提升班组执行力、应变力、凝聚力，并最终形成班组的工作合力！</p>
+        <p class="readmore"><a href="#">继续阅读 &raquo;</a></p>
+      </div>
+    </div>
+    <div class="featured_box"><a href="#"><img src="static/common/img/student_3.jpg" alt="" /></a>
+      <div class="floater">
+        <h2>3. 管理者卓越领导力修炼</h2>
+        <p>很多人对于“领导力”存在理解上的误区，认为领导力就是管理者个人能力和个人魅力，这些固然重要，但是对于组织来说，更重要的是目标，所以领导力必须服务于组织大目标，如果与组织目标背离，那么这种领导力对组织带来的必然是伤害，因此领导力的修炼除了个人魅力和个人能力的提升之外，更要结合组织目标和管理实践才具有实际意义。</p>
+        <p class="readmore"><a href="#">继续阅读 &raquo;</a></p>
+      </div>
+    </div>
+    <div class="featured_box"><a href="#"><img src="static/common/img/student_4.jpg" alt="" /></a>
+      <div class="floater">
+        <h2>4. 团队高效协作与执行管理模式</h2>
+        <p>这一系列的问题困扰着每一个管理者，有没有一种管理模式，可以激活团队和组织，使员工能够创造性工作，而不是被动完成任务；使员工能够主动超越目标，而不是应付考核；《团队高效协作与执行管理模式》从组织、文化、人性层面对企业管理过程中面临的实际问题进行深刻剖析，实现“全员、全方位、全过程”的高效能团队管理模式，推动企业管理升级。</p>
+        <p class="readmore"><a href="#">继续阅读 &raquo;</a></p>
+      </div>
+    </div>
+    <div class="featured_box"><a href="#"><img src="static/common/img/student_5.jpg" alt="" /></a>
+      <div class="floater">
+        <h2>5. 企业应变策略与管理模式升级</h2>
+        <p>面对不断变化的生存环境，该如何应对，已经成为每一个企业家不可逃避的思考，变化是永恒的主题，未来的变化会越来越快，并且很难甚至是无法预测，更难以控制，我们能做的只有积极应变，既然大家都知道要变，为什么有些企业可以应变自如，而有些企业却被宣判了死刑呢？由此可见外部变化考验的不仅是企业老板的洞察力，更需要企业具备实际的应变能力，而企业应变能力的背后是企业卓越管理模式的支撑，企业只有通过管理模式的升级才有可能提升整体应变能力，最终做到以不变应万变！</p>
+        <p class="readmore"><a href="#">继续阅读 &raquo;</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper col3">
+  <div id="homecontent">
+    <div class="fl_left">
+      <div class="column2">
+        <ul>
+          <li>
+            <h2>课程：团队高效协作与执行管理模式</h2>
+            <div class="imgholder"><img src="static/common/img/video_img4.jpg" alt="" /></div>
+            <!-- <p>团队高效协作与执行管理模式</p> -->
+            <p>这一系列的问题困扰着每一个管理者，有没有一种管理模式，可以激活团队和组织，使员工能够创造性工作，而不是被动完成任务；使员工能够主动超越目标，而不是应付考核。</p>
+            <p class="readmore"><a href="#">免费试听 &raquo;</a></p>
+          </li>
+          <li class="last">
+            <h2>课程：卓越班组建设与班组长管理能力提升</h2>
+            <div class="imgholder"><img src="static/common/img/video_img4.jpg" alt="" /></div>
+            <!-- <p>团队高效协作与执行管理模式</p> -->
+            <p>现实中这一系列现象令领导头疼，并严重制约着班组发展与绩效成长，而现象背后反映出的则是班组长在管理中缺乏一套行之有效、能够解决以上诸多实际问题的管理模式和管理方法。</p>
+            <p class="readmore"><a href="#">免费试听 &raquo;</a></p>
+          </li>
+        </ul>
+        <br class="clear" />
+      </div>
+      <div class="column2">
+        <h2><a href="#">华智讯项目介绍</a></h2>
+        <img class="imgl" src="static/common/img/8.jpg" alt="" />
+        <p>This is a W3C standards compliant free website template from <a href="http://www.cssmoban.com/">模板之家</a>.</p>
+        <p>This template is distributed using a <a href="#">Website Template Licence</a>, which allows you to use and modify the template for both personal and commercial use when you keep the provided credit links in the footer. For more CSS templates visit <a href="#">Free Website Templates</a>.</p>
+        <p>Morbitincidunt maurisque eros molest nunc anteget sed vel lacus mus semper. Anterdumnullam interdum eros dui urna consequam ac nisl nullam ligula vestassa.</p>
+        <p>Condimentumfelis et amet tellent quisquet a leo lacus nec augue accumsan. Sagittislaorem dolor ipsum at urna et pharetium malesuada nis consectus odio.</p>
+      </div>
+    </div>
+    <div class="fl_right">
+      <h2><a href="#">师资团队</a></h2>
+      <ul>
+        <li>
+          <div class="imgholder"><a href="#"><img src="static/common/img/杨帆.png" alt="" /></a></div>
+          <p><strong><a href="#">杨   帆   先生</a></strong></p>
+          <p>中国高企委职业教育专业委员会  特聘专家；北京大学经济学院 客座教授；北京大学政府管理学院  客座教授；中国竞争力研究中心特聘专家；</p>
+        </li>
+        <li>
+          <div class="imgholder"><a href="#"><img src="static/common/img/杨帆.png" alt="" /></a></div>
+          <p><strong><a href="#">杨   帆   先生</a></strong></p>
+          <p>中国高企委职业教育专业委员会  特聘专家；北京大学经济学院 客座教授；北京大学政府管理学院  客座教授；中国竞争力研究中心特聘专家；</p>
+        </li>
+        <li>
+          <div class="imgholder"><a href="#"><img src="static/common/img/杨帆.png" alt="" /></a></div>
+          <p><strong><a href="#">杨   帆   先生</a></strong></p>
+          <p>中国高企委职业教育专业委员会  特聘专家；北京大学经济学院 客座教授；北京大学政府管理学院  客座教授；中国竞争力研究中心特聘专家；</p>
+        </li>
+        <li>
+          <div class="imgholder"><a href="#"><img src="static/common/img/杨帆.png" alt="" /></a></div>
+          <p><strong><a href="#">杨   帆   先生</a></strong></p>
+          <p>中国高企委职业教育专业委员会  特聘专家；北京大学经济学院 客座教授；北京大学政府管理学院  客座教授；中国竞争力研究中心特聘专家；</p>
+        </li>
+        <li class="last">
+          <div class="imgholder"><a href="#"><img src="static/common/img/杨帆.png" alt="" /></a></div>
+          <p><strong><a href="#">杨   帆   先生</a></strong></p>
+          <p>中国高企委职业教育专业委员会  特聘专家；北京大学经济学院 客座教授；北京大学政府管理学院  客座教授；中国竞争力研究中心特聘专家；</p>
+        </li>
+      </ul>
+    </div>
+    <br class="clear" />
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<!-- <div class="wrapper col4">
+  <div id="footer">
+    <div class="footbox">
+      <h2>Lacus interdum</h2>
+      <ul>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li><a href="#">Praesent et eros</a></li>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li class="last"><a href="#">Praesent et eros</a></li>
+      </ul>
+    </div>
+    <div class="footbox">
+      <h2>Lacus interdum</h2>
+      <ul>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li><a href="#">Praesent et eros</a></li>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li class="last"><a href="#">Praesent et eros</a></li>
+      </ul>
+    </div>
+    <div class="footbox">
+      <h2>Lacus interdum</h2>
+      <ul>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li><a href="#">Praesent et eros</a></li>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li class="last"><a href="#">Praesent et eros</a></li>
+      </ul>
+    </div>
+    <div class="footbox">
+      <h2>Lacus interdum</h2>
+      <ul>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li><a href="#">Praesent et eros</a></li>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li class="last"><a href="#">Praesent et eros</a></li>
+      </ul>
+    </div>
+    <div class="footbox last">
+      <h2>Lacus interdum</h2>
+      <ul>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li><a href="#">Praesent et eros</a></li>
+        <li><a href="#">Lorem ipsum dolor</a></li>
+        <li><a href="#">Suspendisse in neque</a></li>
+        <li class="last"><a href="#">Praesent et eros</a></li>
+      </ul>
+    </div>
+    <br class="clear" />
+  </div>
+</div> -->
+<!-- ####################################################################################################### -->
+<div class="wrapper col5">
+  <div id="copyright">
+    <p class="fl_left">CopyRight &copy; 2011-2013 华智讯教育培训公司 All Rights Reserved - <a href="#">www.hzx.com</a></p>
+    <p class="fl_right">联系电话：010-xxxxxxxx 传真：010-xxxxxxxx</a></p>
+    <br class="clear" />
+  </div>
+</div>
+</body>
+</html>
