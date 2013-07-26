c0dc0fb2cc1ab7ed711c4d4ccccaf9b9c35e6461
WT <wangte168@sina.com>
2013-07-27 00:06:49 +0800

    修改部分

--- application/views/courselist.php
+++ application/views/courselist.php
@@ -1,30 +1,18 @@
+<div id="myposition" class="box"><p><a href="#">首页</a>&nbsp&gt&nbsp<a href="#">主打课程</a></p></div>
 <div class="box">
-<div id="myposition"><p><a href="#">首页</a>&nbsp&gt&gt&nbsp主打课程</p></div>
-<div id="teacher_detail">
-	<div class="title_4">
-		<p>主打课程</p>
+	<div id="teacher_detail">
+		<div class="course_list">
+			<ul>
+				<?php $i = 0;?>
+				<?php foreach ($courses as $c):?>
+				<li <?php if($i/4%2==1) echo 'class="bg_249"'; ?>>
+					<a title="<?php  echo $c['name'];?>" href="<?php echo base_url('/courses/?id=' . $c['id']); ?>"><img src="<?php echo base_url($c['photo']); ?>" width="147" height="101" /></a>
+					<a title="<?php  echo $c['name'];?>" href="<?php echo base_url('/courses/?id=' . $c['id']); ?>"><p class="t_center"><?php  echo $c['name'];?></p></a>
+					<p class="course_remark"><span></span>&nbsp主讲人：<a href="#"><?php  echo $c['professor'];?></a></p><!-- 主讲人-链接？ -->
+				</li>
+				<?php $i++;?>
+				<?php endforeach ?>
+				<div class="clear"></div>
+			</ul>
+		</div>
 	</div>
-	<div id="share_message">
-		<p>分享到：</p>
-		<a rel="nofollow" href="javascript:void((function(s,d,e){try{}catch(e){}var%20f='http://v.t.sina.com.cn/share/share.php?',u=d.location.href,p=['url=',e(u),'&title=',e(d.title),'&appkey=1392530042'].join('');function a(){if(!window.open([f,p].join(''),'mb',['toolbar=0,status=0,resizable=1,width=620,height=450,left=',(s.width-620)/2,',top=',(s.height-450)/2].join('')))u.href=[f,p].join('');};if(/Firefox/.test(navigator.userAgent)){setTimeout(a,0)}else{a()}})(screen,document,encodeURIComponent));"><img src="<?php echo base_url('static/common/img/sinaicon.gif') ?>" alt="分享到新浪微博" title="分享到新浪微博" border='0'></a>
-		<a href="javascript:void(0)" onclick="{ var _t = encodeURI(document.title);  var _url = encodeURI(window.location); var _appkey = '333cf198acc94876a684d043a6b48e14'; var _site = encodeURI; var _pic = ''; var _u = 'http://v.t.qq.com/share/share.php?title='+_t+'&url='+_url+'&appkey='+_appkey+'&site='+_site+'&pic='+_pic; window.open( _u,'转播到腾讯微博', 'width=700, height=580, top=180, left=320, toolbar=no, menubar=no, scrollbars=no, location=yes, resizable=no, status=no' );  };" ><IMG border=0 title="转播到腾讯微博" src="<?php echo base_url('static/common/img/tencenticon.png') ?>" width=16 height=16></a>
-		<a href="tencent://message/?uin=22062284" alt="杨老师" title="杨老师"><img src="http://wpa.qq.com/pa?p=1:22062284:4" border="0" /></a>
-		<!-- <a href="javascript:void((function(s,d,e){if(/renren\.com/.test(d.location))return;var f=’http://share.renren.com/share/buttonshare?link=’,u=d.location,l=d.title,p=[e(u),'&title=',e(l)].join(”);function%20a(){if(!window.open([f,p].join(”),’xnshare’,['toolbar=0,status=0,resizable=1,width=626,height=436,left=',(s.width-626)/2,',top=',(s.height-436)/2].join(”)))u.href=[f,p].join(”);};if(/Firefox/.test(navigator.userAgent))setTimeout(a,0);else%20a();})(screen,document,encodeURIComponent));" title="分享到人人网"><img border=0 title="转播到人人网" src="static/common/img/renren.png" width=16 height=16 /></a> -->
-		<a class="fav_renren" rel="nofollow" href="javascript:window.open('http://share.renren.com/share/buttonshare.do?link='+encodeURIComponent(document.location.href)+'&amp;title='+encodeURIComponent(document.title));void(0)"><img border=0 title="转播到人人网" src="<?php echo base_url('static/common/img/renren.png')?>" width=16 height=16 /></a>
-		<div class="clear"></div>
-	</div>
-	<div class="course_list">
-		<ul>
-			<?php $i = 0;?>
-			<?php foreach ($courses as $c):?>
-			<li <?php if($i/4%2==1) echo 'class="bg_249"'; ?>>
-				<a title="<?php  echo $c['name'];?>" href="<?php echo base_url('/courses/?id=' . $c['id']); ?>"><img src="<?php echo base_url($c['photo']); ?>" width="147" height="101" /></a>
-				<a title="<?php  echo $c['name'];?>" href="<?php echo base_url('/courses/?id=' . $c['id']); ?>"><p class="t_center"><?php  echo $c['name'];?></p></a>
-				<p class="course_remark"><span></span>&nbsp主讲人：<a href="#"><?php  echo $c['professor'];?></a></p><!-- 主讲人-链接？ -->
-			</li>
-			<?php $i++;?>
-			<?php endforeach ?>
-			<div class="clear"></div>
-		</ul>
-	</div>
-</div>
