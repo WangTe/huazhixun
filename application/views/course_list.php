65c6c7756651130e5f1c5e63c8b07d5e30737515
WT <wangte168@sina.com>
2013-07-27 00:08:15 +0800

    修改course_list

--- application/views/course_list.php
+++ application/views/course_list.php
@@ -1,32 +1,18 @@
-
-	<div id="content_right" class="list_content_right">
-    	<div class="title_2">
-            <p><?php echo $title; ?></p>
-        </div>
-        <div>
-            <div class="list_content_right list_courses">
-                <table class="t_center" border="0" align="center" cellpadding="0" cellspacing="0">
-                    <?php foreach ($courses as $row): ?>
-                    <tr>
-                        <td align="left"><p><a href="<?php echo base_url('course/?cid=' . $row['cid']); ?>"><?php echo $row['name']?></a></p></td>
-                        <td width="70"><?php echo $row['level']; ?></td>
-                        <td width="80"><?php echo $row['time']; ?></td>
-                        <td width="80"><?php echo $row['price']; ?></td>
-                        <td width="40"><a href="<?php echo base_url('apply/?cid=' . $row['cid']); ?>">报名</a></td>
-                    </tr>
-                    <?php endforeach; ?>
-                </table>
-            </div>
-            <div id="page">
-            	<?php echo $page_html; ?>
-            </div>
-        </div>
-    </div>
-    <div class="clear">
-    </div>
-    <div class="content_foot1"> 	
-    </div>
-</div>
-
-<div class="content_foot2">
-</div>
+<div id="myposition" class="box"><p><a href="#">首页</a>&nbsp&gt&nbsp<a href="#">主打课程</a></p></div>
+<div class="box">
+	<div id="teacher_detail">
+		<div class="course_list">
+			<ul>
+				<?php $i = 0;?>
+				<?php foreach ($courses as $c):?>
+				<li <?php if($i/4%2==1) echo 'class="bg_249"'; ?>>
+					<a title="<?php  echo $c['name'];?>" href="<?php echo base_url('/courses/?id=' . $c['id']); ?>"><img src="<?php echo base_url($c['photo']); ?>" width="147" height="101" /></a>
+					<a title="<?php  echo $c['name'];?>" href="<?php echo base_url('/courses/?id=' . $c['id']); ?>"><p class="t_center"><?php  echo $c['name'];?></p></a>
+					<p class="course_remark"><span></span>&nbsp主讲人：<a href="#"><?php  echo $c['professor'];?></a></p><!-- 主讲人-链接？ -->
+				</li>
+				<?php $i++;?>
+				<?php endforeach ?>
+				<div class="clear"></div>
+			</ul>
+		</div>
+	</div>
