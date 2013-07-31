34be1ec68ad5688feb50b0ff19257bcf35adb322
WT <wangte168@sina.com>
2013-07-31 21:27:22 +0800

    修改杨老师QQ号码

--- application/views/index.php
+++ application/views/index.php
@@ -109,7 +109,7 @@
 			<p>分享到：</p>
 			<a rel="nofollow" href="javascript:void((function(s,d,e){try{}catch(e){}var%20f='http://v.t.sina.com.cn/share/share.php?',u=d.location.href,p=['url=',e(u),'&title=',e(d.title),'&appkey=1392530042'].join('');function a(){if(!window.open([f,p].join(''),'mb',['toolbar=0,status=0,resizable=1,width=620,height=450,left=',(s.width-620)/2,',top=',(s.height-450)/2].join('')))u.href=[f,p].join('');};if(/Firefox/.test(navigator.userAgent)){setTimeout(a,0)}else{a()}})(screen,document,encodeURIComponent));"><img src="static/common/img/sinaicon.gif" alt="分享到新浪微博" title="分享到新浪微博" border='0'></a>
 			<a href="javascript:void(0)" onclick="{ var _t = encodeURI(document.title);  var _url = encodeURI(window.location); var _appkey = '333cf198acc94876a684d043a6b48e14'; var _site = encodeURI; var _pic = ''; var _u = 'http://v.t.qq.com/share/share.php?title='+_t+'&url='+_url+'&appkey='+_appkey+'&site='+_site+'&pic='+_pic; window.open( _u,'转播到腾讯微博', 'width=700, height=580, top=180, left=320, toolbar=no, menubar=no, scrollbars=no, location=yes, resizable=no, status=no' );  };" ><IMG border=0 title="转播到腾讯微博" src="static/common/img/tencenticon.png" width=16 height=16></a>
-			<a href="tencent://message/?uin=251138775" alt="杨老师" title="杨老师"><img src="http://wpa.qq.com/pa?p=1:251138775:4" border="0" /></a>
+			<a href="tencent://message/?uin=1246134763" alt="杨老师" title="杨老师"><img src="http://wpa.qq.com/pa?p=1:1246134763:4" border="0" /></a>
 			<div class="clear"></div>
 		</div>
 	</div>
